8f674a04a53996a111fbad89ecf6260df91da83b
DAESUNG KIM <kds070531@naver.com>
2025-11-18 17:01:29 +0900

    KIPS/디자인 완성 세부적인 반응형만 제작하면 끝

--- kips/resource/js/main.js
+++ kips/resource/js/main.js
@@ -2,11 +2,11 @@ function headerPadding() {
     let header = document.querySelector('header')
     document.body.style.paddingTop = header.offsetHeight + 'px'
 }
-$('.navi li, .sub-bg').mouseenter(function() {
-    $('.sub-menu, .sub-bg').stop().slideDown()
+$('.navi li, .sub-bg-wrap').mouseenter(function() {
+    $('.sub-menu, .sub-bg-wrap').stop().slideDown()
 })
-$('.navi li, .sub-bg').mouseleave(function() {
-    $('.sub-menu, .sub-bg').stop().slideUp()
+$('.navi li, .sub-bg-wrap').mouseleave(function() {
+    $('.sub-menu, .sub-bg-wrap').stop().slideUp()
 })
 window.addEventListener('load', headerPadding)
 window.addEventListener('resize', headerPadding)
