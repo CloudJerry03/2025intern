b1defa50bb95753cd08f92fcd698bc3ce4998171
DAESUNG KIM <kds070531@naver.com>
2025-12-18 11:09:13 +0900

    kips/서브페이지 학회 공지사항 완성 + 헤더 푸터 모듈화

--- kips/resource/js/main.js
+++ kips/resource/js/main.js
@@ -1,47 +1,45 @@
-function headerPadding() {
-    let header = document.querySelector('header')
-    // header가 존재할 때만 paddingTop을 설정
-    if (header) {
-        document.body.style.paddingTop = header.offsetHeight + 'px'
-    }
-}
+$(document).ready(function () {
+    $("#header").load("./resource/include/header.html", function () {
+        $('.navi li, .sub-bg-wrap').on('mouseenter', function () {
+            $('.sub-menu, .sub-bg-wrap').stop().slideDown(300);
+        });
 
-// 데스크톱 서브 메뉴 슬라이드 (jQuery)
-$('.navi li, .sub-bg-wrap').on('mouseenter', function () {
-    $('.sub-menu, .sub-bg-wrap').stop().slideDown(300); // 300ms 애니메이션 추가
-});
+        $('.navi li, .sub-bg-wrap').on('mouseleave', function () {
+            $('.sub-menu, .sub-bg-wrap').stop().slideUp(300);
+        });
+        $('.mobile > i').on('click', function () {
+            $('.mobile .menu').slideToggle(300);
+            $(this).toggleClass('fa-bars fa-close');
+        });
 
-$('.navi li, .sub-bg-wrap').on('mouseleave', function () {
-    $('.sub-menu, .sub-bg-wrap').stop().slideUp(300); // 300ms 애니메이션 추가
-});
+        $('.mobile .menu li .main-menu').on('click', function () {
+            $(this).children('i').toggleClass('fa-angle-down fa-angle-up');
+            $(this).closest('li').children('.sub-menu').slideToggle(300);
+        });
 
-// 페이지 로드 및 리사이즈 시 헤더 패딩 설정
-window.addEventListener('load', headerPadding);
-window.addEventListener('resize', headerPadding);
+        window.addEventListener('resize', function () {
+            if (window.innerWidth > 767) {
+                $('.mobile .menu').stop(true, true).hide();
+                $('.mobile > i')
+                    .removeClass('fa-close')
+                    .addClass('fa-bars');
+                $('.mobile .sub-menu').hide();
+                $('.mobile .menu i')
+                    .removeClass('fa-angle-up')
+                    .addClass('fa-angle-down');
+            }
+        });
 
-// 모바일 메뉴 토글 (jQuery)
-$('.mobile > i').on('click', function () {
-    $('.mobile .menu').slideToggle(300);
-    $(this).toggleClass('fa-bars fa-close');
-});
+        function headerPadding() {
+            let header = document.querySelector('header')
+            if (header) {
+                document.body.style.paddingTop = header.offsetHeight + 'px'
+            }
+        }
 
-// 모바일 서브 메뉴 토글 (jQuery) - .flex-row.align-center 대신 .main-menu.flex-center 사용
-$('.mobile .menu li .main-menu').on('click', function () {
-    // 자식 아이콘 토글
-    $(this).children('i').toggleClass('fa-angle-down fa-angle-up');
-    // 가장 가까운 li의 자식 sub-menu 토글
-    $(this).closest('li').children('.sub-menu').slideToggle(300);
-});
+        headerPadding()
+        window.addEventListener('resize', headerPadding);
 
-window.addEventListener('resize', function () {
-    if (window.innerWidth > 767) {
-        $('.mobile .menu').stop(true, true).hide();
-        $('.mobile > i')
-            .removeClass('fa-close')
-            .addClass('fa-bars');
-        $('.mobile .sub-menu').hide();
-        $('.mobile .menu i')
-            .removeClass('fa-angle-up')
-            .addClass('fa-angle-down');
-    }
+    });
+    $("#footer").load("./resource/include/footer.html");
 });
